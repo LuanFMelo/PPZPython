c120fe5aaa55090b6e0465e960eb3ce8a5dd1287
LuanFMelo <46304677+LuanFMelo@users.noreply.github.com>
2021-01-04 01:27:53 -0300

    Add-Break-exercises

--- classroom-exercises/TWP175 Break - 2.py
+++ classroom-exercises/TWP175 Break - 2.py
@@ -13,3 +13,15 @@ while True:
         break
     soma = soma + x
 print(f'Soma: {soma}')
+
+
+#Mudar codigo para calcular média
+
+soma = 0
+
+while True:
+    x = int(input('n (zero sai): '))
+    if x == 0:
+        break
+    media = (soma + x) / x
+print(f'Média: {media}')
